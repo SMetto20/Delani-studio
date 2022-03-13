143be2be3b8cf97e503d6594b39495c3d7eaede9
smetto20 <sandra.metto@student.moringaschool.com>
2022-03-13 18:43:52 +0300

    adds dynamics to portfolio

---
+++ js/main.js
@@ -0,0 +1,71 @@
+var filterval= 'blur(3px)'
+var filterval2= 'blur(0px)'
+
+$(".work4").mouseover(function () {
+    $(".goodlife").show();
+    $(".work4").css('filter',filterval)
+})
+$(".work4").mouseleave(function(){
+    $(".goodlife").hide()
+    $(".work4").css('filter',filterval2)
+})
+$(".work3").mouseover(function () {
+    $(".ontario").show();
+    $(".work3").css('filter',filterval)
+    
+})
+$(".work3").mouseleave(function(){
+    $(".ontario").hide()
+    $(".work3").css('filter',filterval2)
+})
+$(".work2").mouseover(function () {
+    $(".sunshine").show();
+    $(".work2").css('filter',filterval)
+})
+$(".work2").mouseleave(function(){
+    $(".sunshine").hide()
+    $(".work2").css('filter',filterval2)
+})
+$(".work1").mouseover(function () {
+    $(".baywatch").show();
+    $(".work1").css('filter',filterval)
+})
+$(".work1").mouseleave(function(){
+    $(".baywatch").hide()
+    $(".work1").css('filter',filterval2)
+})
+
+$(".work5").mouseover(function () {
+    $(".tranquil").show();
+    $(".work5").css('filter',filterval)
+})
+$(".work5").mouseleave(function(){
+    $(".tranquil").hide()
+    $(".work5").css('filter',filterval2)
+})
+
+$(".work6").mouseover(function () {
+    $(".icecream").show();
+    $(".work6").css('filter',filterval)
+})
+$(".work6").mouseleave(function(){
+    $(".icecream").hide()
+    $(".work6").css('filter',filterval2)
+})
+$(".work7").mouseover(function () {
+    $(".coffee").show();
+    $(".work7").css('filter',filterval)
+})
+$(".work7").mouseleave(function(){
+    $(".coffee").hide();
+    $(".work7").css('filter',filterval2)
+})
+$(".work8").mouseover(function () {
+    $(".burgers").show();
+    $(".work8").css('filter',filterval)
+})
+$(".work8").mouseleave(function(){
+    $(".burgers").hide()
+$(".work8").css('filter',filterval2)
+   
+})
